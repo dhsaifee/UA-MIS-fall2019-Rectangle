a04baf95aa07a4017abbda3d509189ded9eed22c
dhsaifee <dhsaifee@cba.ua.edu>
2019-10-21 17:37:17 -0500

    Add project files.

---
+++ Rectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of rectangles: " + Rectangle.GetCount());
+
+            Rectangle myRectangle1 = new Rectangle();
+            Rectangle.IncCount();
+            double area1 = myRectangle1.GetArea();
+            Console.WriteLine("Rectangle1 length: " + myRectangle1.GetLength() + ", Rectangle1 width: " + myRectangle1.GetWidth());
+            Console.WriteLine("Area for Rectangle1: " + area1);
+            Console.WriteLine("Number of rectangles: " + Rectangle.GetCount());
+
+            Rectangle myRectangle2 = new Rectangle(10, 5.5);
+            Rectangle.IncCount();
+            double area2 = myRectangle2.GetArea();
+            Console.WriteLine("Rectangle2 length: " + myRectangle2.GetLength() + ", Rectangle2 width: " + myRectangle2.GetWidth());
+            Console.WriteLine("Area for Rectangle2: " + area2);
+            Console.WriteLine("Number of rectangles: " + Rectangle.GetCount());
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        private static int count;
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double GetLength()
+        {
+            return length;
+        }
+
+        public void SetLength(double length)
+        {
+            this.length = length;
+        }
+
+        public double GetWidth()
+        {
+            return width;
+        }
+
+        public void SetWidth(double width)
+        {
+            this.width = width;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public static int GetCount()
+        {
+            return count;
+        }
+
+        public static void SetCount(int count)
+        {
+            Rectangle.count = count;
+        }
+
+        public static void IncCount()
+        {
+            count++;
+        }
+
+    }
+}
